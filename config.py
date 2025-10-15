4387c38089d2247165be4480a00fcd0c7cbd8b27
Dbhosale146 <75349936+Dbhosale146@users.noreply.github.com>
2025-10-15 13:56:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-EURI_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzZDk1ZjRjNC03ZDc1LTRlMjItODY1Mi0wNGViMjkxMjU1Y2UiLCJlbWFpbCI6ImRueWFuZXNod2FyYmhvc2FsZTExMUBnbWFpbC5jb20iLCJpYXQiOjE3Mzg0ODk0NjQsImV4cCI6MTc3MDAyNTQ2NH0.frcheb8DJU_9AOCaTHdLDJ9Ud9OWrk1Gjp-NpV3dSp8"
+EURI_API_KEY = "euri-9d585e30a6c430882f4ce91300bf65380ee40422669bf19b3b2d7df6b6a487be"
 EURI_API_URL  = "https://api.euron.one/api/v1/euri/alpha/chat/completions"
 MODEL_NAME  = "gpt-4.1-nano"
 DATABASE_URI = "postgresql://neondb_owner:npg_GPhIBZdv3XL5@ep-rough-cake-a8ic91hs-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
+
